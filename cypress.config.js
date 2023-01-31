395928ed960c29744664d2ae44c898aa8fc389df
shahing <arrestshahin@gmail.com>
2023-01-31 22:25:55 +0000

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,46 @@
+const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.google.com",
+    viewportHeight: 900,
+    viewportWidth: 1400,
+    specPattern: "**/*.feature",
+    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents,
+  },
+});
